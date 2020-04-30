24831a277e95a3bab2220fc4496b6de0d382f2ab
Genvara <nadjetissam@gmail.com>
2020-04-30 01:10:52 +0100

    Wall O' Names and Communities

---
+++ Wall O' Names and Communities™.js
@@ -0,0 +1,37 @@
+console.log("Psst, you might wanna check this JS script for Wall O' Names and Communities™")
+
+
+/* I wanted to have a place where i put names of some of my friends who helped me all these years, whom i stayed with for a
+very long time, and love very much, i might not have included everyone, but ill try to include everyone i know.
+
+===============================================================================================================================
+
+MD - SHADOWMASTER/KAGE - CLUMSY - ISTEALTHY - TROPICALCYCLONEALERT - REX - ISTEALTHY - JOE487 - MAD MAX - ABC64 - PIKE
+TREP - SUBZEROTO - GEMNOR - SHINE - IXENOGEN - ZINO - DOT - JOHNNYWUZZ - ORIGINALLYBLAZE/FIRENATION - DARKLIGHT - APICH
+BLUDUCKIE - JOHN RONALD PEREZ - LYCANROD - DROP - GECKO - SLEEPYREQUIEM - FRENCHBOI - CUPBEEM2487 - PHANTOMDETECTIVE
+DARK SHADOW - CY1IA - ZEZO - IXZ - HCC - DARKTHUNDER - SUPPIE - COOKIE - DARKAINE - XZENO - BONESHRIMP/NAWAFGAMER -
+IFORG - DOGER - BREADUCK - DAWGRADOGU/DOGUTSUNE - FADY - BLACOISO - TNTSQUID - FISHCAKE47 - HOUDINI and more...
+
+===============================================================================================================================
+
+Im now going to start mentioning people as in communities, because writing names would be a pain, don't be sad because
+i didn't put your name up there, it's just too much, i will update the Wall O' Names™ as time goes on.
+Thanks to:
+The Golden Organization: Met some people who are in the ROBLOX anime fighting community, they play a bunch of jojo fan
+games, soulshatter is a cool fighting game too, the people are very nice :))
+The .IO Community: Yes, every single clan that still exists/or existed before, thank you, without you i wouldn't able to
+have a lot of friends and meet bunch of cool people, especially in the year 2016-2018, or even now, love you guys <3.
+BORED and SBK (sinbad knights for short): While SBK being a .IO clan, there were a lot of talented people of there, from
+artists/designers, programmers, and others... some of them are very known today :)
+Nightosphere: Possibly the best community in the middle east and north africa, great youtube content that is not like the
+usual arabian content that you see nowadays, with amazing artists posting their work in the discord server.
+Dogu's Funhouse: While being based on ROBLOX, there's a ton of great people on there, also like your work dogu <3.
+The Discord Mod Volunteers: Shoutouts to them for moderating HUGE discord servers, we are talking about servers that reach
+100k members, and moderating official discord servers.
+That's it for Wall O' Communities i guess.
+
+===============================================================================================================================
+
+Again, i will update this at any time, i hope github doesn't shut down and all of this will be gone to waste, Love all of you.
+-Genvara
+*/
